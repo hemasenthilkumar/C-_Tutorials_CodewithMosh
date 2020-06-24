10a8e3612fbafc6d16c8afcf9cbf4a205efd3cc0
hemasenthilkumar <hema.senthilkumar@outlook.com>
2020-06-24 14:21:50 +0530

    Add project files.

---
+++ Classes_CodewithMosh/Program.cs
@@ -0,0 +1,33 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_CodewithMosh
+{
+    class Program
+    {
+        public class Person
+        {
+            public string Name;
+            public void Introduce(string to)
+            {
+                Console.WriteLine("Hi {0} I am {1}", to, Name);
+            }
+            public static Person Parse(string str)
+            {
+                var p = new Person();
+                p.Name = str;
+                return p;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var p = Person.Parse("Vasanth");
+            p.Introduce("Hema");
+           
+            Console.ReadKey();
+        }
+    }
+}
