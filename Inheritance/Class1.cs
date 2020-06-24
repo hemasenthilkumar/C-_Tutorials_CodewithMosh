32166368469dbb20a66018339eb9eedd2edc5e4c
hemasenthilkumar <hema.senthilkumar@outlook.com>
2020-06-24 19:06:30 +0530

    Day1-Intermediate

---
+++ Inheritance/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+    public class PresentationObject
+    {
+        public int width { get; set; }
+        public int Height { get; set; }
+        public void Copy()
+        {
+            Console.WriteLine("Copied");
+        }
+        public void Duplicate()
+        {
+            Console.WriteLine("duplicated");
+        }
+    }
+}
+ 
---
+++ Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+        public void addhyperlink (string url)
+        {
+            Console.WriteLine("Hyperlink added:{0}", url);
+        }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.width = 100;
+            text.addhyperlink("www.google.com");
+            Console.ReadKey();
+        }
+
+    }
+}
